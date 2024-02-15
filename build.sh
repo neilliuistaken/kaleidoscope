38cc67c9291da75ff646c49f8eee2a9a86185fa9
Neil <neilliu1994@gmail.com>
2024-02-15 15:33:40 +0000

    enhancement: better build script
    - build script supports options and positional arguments
    - get rid of llvm-14 specific binding

--- build.sh
+++ build.sh
@@ -1,16 +1,82 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
+VERBOSE_MODE=0
 ACTION="build"
-if [[ ! -z "$1" ]]; then
-    ACTION="$1"
-fi
-
 PROJECT_BUILD_TYPE="Debug"
-if [[ ! -z "$2" ]]; then
-    PROJECT_BUILD_TYPE="$2"
-fi
+CMAKE_ARGUMENTS=()
+
+Build () {
+    echo "Build type: ${PROJECT_BUILD_TYPE}"
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    cd "$SCRIPT_DIR"
+    mkdir -p build
+    cd build
+    cmake ../ -GNinja -DCMAKE_BUILD_TYPE=Debug
+    ninja all
+}
+
+Clean () {
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    cd "$SCRIPT_DIR"
+    rm -rf build
+    echo "Build files are cleaned."
+}
+
+CURRENT_POSITIONAL_ARGUMENT_INDEX=0
+
+DOUBLE_DASH_IS_PROCESSED=0
+
+# $1 is argument value
+HandlePositionalArgument () {
+    if [[ "$DOUBLE_DASH_IS_PROCESSED" == "1" ]]; then
+        CMAKE_ARGUMENTS+=("$1")
+        return 0
+    fi
+    if [[ "$CURRENT_POSITIONAL_ARGUMENT_INDEX" == "0" ]]; then
+        ACTION="$1"
+    else
+        echo "Too many positional arguments! argument '$1' is ignored."
+    fi
+    CURRENT_POSITIONAL_ARGUMENT_INDEX=$((CURRENT_POSITIONAL_ARGUMENT_INDEX+1))
+}
+
+# $1 is argument name
+# $2 is argument value, if applicable
+HandleOptionArgument () {
+    case "$1" in
+        "--help")
+            echo "No help yet."
+            exit 0
+            ;;
+        "--")
+            DOUBLE_DASH_IS_PROCESSED=1
+            ;;
+        "--debug")
+            PROJECT_BUILD_TYPE="Debug"
+            ;;
+        "--release")
+            PROJECT_BUILD_TYPE="Release"
+            ;;
+        "--verbose" | "-v")
+            VERBOSE_MODE=1
+            ;;
+        *)
+            echo "Unrecognized option: $1"
+            exit 1
+            ;;
+    esac
+}
+
+for var in "$@"
+do
+    ARG_NAME=${var%=*}
+    ARG_VALUE=${var##*=}
+    if [[ "$DOUBLE_DASH_IS_PROCESSED" == "0" && "$ARG_NAME" == -* ]]; then
+        HandleOptionArgument "$ARG_NAME" "$ARG_VALUE"
+    else
+        HandlePositionalArgument "$var"
+    fi
+done
 
 case "${ACTION}" in
     "build")
@@ -18,7 +84,10 @@ case "${ACTION}" in
         cd "$SCRIPT_DIR"
         mkdir -p build
         cd build
-        cmake ../ -GNinja -DCMAKE_BUILD_TYPE=Debug
+        if [[ "$VERBOSE_MODE" == "1" ]]; then
+            echo "cmake ../ -GNinja -DCMAKE_BUILD_TYPE=$PROJECT_BUILD_TYPE ${CMAKE_ARGUMENTS[*]}"
+        fi
+        cmake ../ -GNinja -DCMAKE_BUILD_TYPE=$PROJECT_BUILD_TYPE ${CMAKE_ARGUMENTS[*]}
         ninja all
         ;;
     "clean")
